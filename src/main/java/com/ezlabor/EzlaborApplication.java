f9a2a78e491837150183ebbaae1862d4a3259a24
Sebastian <sebastian.noli@gmail.com>
2021-05-08 14:41:11 -0500

    Revert "heroku deploy"
    This reverts commit b1eeaab571740c3879d2d904b531d59bb36f1581.

--- src/main/java/com/ezlabor/EzlaborApplication.java
+++ src/main/java/com/ezlabor/EzlaborApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EzlaborApplication {
