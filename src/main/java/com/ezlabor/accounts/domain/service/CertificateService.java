3c29aaa41da5e673596d6f431dd8f1f148b9a853
Juan de Dios Quiroz <u201910127@upc.edu.pe>
2021-05-08 02:59:01 -0500

    Test:CertificateServiceTest

--- src/main/java/com/ezlabor/accounts/domain/service/CertificateService.java
+++ src/main/java/com/ezlabor/accounts/domain/service/CertificateService.java
@@ -5,10 +5,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CertificateService {
-    List<Certificate> getAllCertificatesByKnowledgeId(Long knowledgeId, Pageable pageable);
-    Certificate getCertificateByIdAndKnowledgeId(Long certificateId, Long knowledgeId);
+    List<Certificate> getAllCertificatesByKnowledgeId(Long knowledgeId);
+    Optional<Certificate> getCertificateByIdAndKnowledgeId(Long certificateId, Long knowledgeId);
     Certificate createCertificate(Long knowledgeId, Certificate certificate);
     Certificate updateCertificate(Long knowledgeId, Long certificateId, Certificate certificateDetails);
     ResponseEntity<?> deleteCertificate(Long knowledgeId, Long certificateId);
