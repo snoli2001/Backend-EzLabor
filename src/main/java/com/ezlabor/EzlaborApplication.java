323d614d1e5b58d274a646d8590aa35a77a5ecb2
Sebastian <sebastian.noli@gmail.com>
2021-06-06 21:50:12 -0500

    feat: heroku deploy

--- src/main/java/com/ezlabor/EzlaborApplication.java
+++ src/main/java/com/ezlabor/EzlaborApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
